9e973405019917aff01dc34ae9b9f6ac59390e87
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2025-10-02 23:04:13 +0800

    Create Player Movement.cpp

---
+++ Activity4/Conditionals Problem Set/Player Movement.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int x = 0, y = 0;
+    string move;
+    cout << "Enter move (W/A/S/D): ";
+    cin >> move;
+
+    if (move == "W" || move == "w") {
+        y += 1;
+    } else if (move == "S" || move == "s") {
+        y -= 1;
+    } else if (move == "A" || move == "a") {
+        x -= 1;
+    } else if (move == "D" || move == "d") {
+        x += 1;
+    } else {
+        cout << "Invalid move!" << endl;
+    }
+
+    cout << "The location of the player is ( " << x << ", " << y << " )" << endl;
+
+    return 0;
+}
